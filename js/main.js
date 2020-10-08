d16f7d13c641f79e8df37d57bfc92655a49f6f58
enbre <noahbrezel@gmail.com>
2020-10-08 15:00:23 -0700

    All functions and variables out of game object.

--- js/main.js
+++ js/main.js
@@ -29,37 +29,37 @@ let container = document.createElement('div');
 container.setAttribute('class', 'container');
 document.querySelector('.game-frame').appendChild(container)
 
-// /*
-let game = {
-    // number of player moves:
-    moves: 0,
-    // array representing the game board locations:
-    gridArr: [],
-    // total number of cells in grid array
-    gridArrLen: 0,
-    // length of one side of the grid
-    // array used to store image objects with source, value, and id keys:
-    cellArr: [],
 
-    emptyLoc: 0,
+
+// number of player moves:
+let moves= 0;
+// array representing the game board locations:
+let gridArr= [];
+// total number of cells in grid array
+let gridArrLen= 0;
+// length of one side of the grid
+let gridSide = Math.sqrt(gridArrLen)
+// array used to store image objects with source, value, and id keys:
+let cellArr= [];
     
-    // populate the grid array with indices:
-    fillGridArr: function(){
-        for (let i =0; i<9;i++){
-            this.gridArr.push(i)
-            this.gridArrLen++
-        }
-    },
+let emptyLoc= 0;
 
-    // creates divs containers for images:
-    //apply images with innerHTML- though this doesn't seem to be working
-    createCellDivs: function () {
-        // create cells
-        for (let i = 0; i < 9; i++) {
-            let cell = document.createElement('div');
+// populate the grid array with indices:
+function fillGridArr() {
+    for (let i = 0; i < 9; i++) {
+        gridArr.push(i)
+        gridArrLen++
+    }
+};
+
+// creates divs containers for images:
+function createCellDivs () {
+// create cells
+    for (let i = 0; i < 9; i++) {
+        let cell = document.createElement('div');
             cell.setAttribute('class', 'cell');
             cell.setAttribute('id', `cellDiv${i}`)
-            let cellImg = document.createElement('img')
+        let cellImg = document.createElement('img')
             cellImg.setAttribute('src', `./images/empty.png`);
             cellImg.setAttribute('class', 'cell-Img');
             cellImg.setAttribute('classList', 'cell-Empty');
@@ -67,116 +67,104 @@ let game = {
             container.appendChild(cell);
             cell.appendChild(cellImg);
 
-        }
-    },
-
-    // populates cells array with object with image source, value, and id (is ID useful for the image?):
-    getCells: function () {
-        // this.createCells()
-        for (let i = 1; i < 9; i++) {
-            this.cellArr.push({ src: `./images/image${i}.png`, value: i, id: `img${i}` })
-        }
-        this.cellArr.push({ src: `./images/empty.png`, value: 0, id: `img0` })
-        // console.log(this.cells)
-    },
-
-    // shuffles cells array so they are in random order, done at the start of the game:
-    shuffleCells: function () {
-        // this.getCells()
-        for (let i = (this.cellArr.length - 1); i > 0; i--) {
-            x = (Math.floor(Math.random() * (i)))
-            temp = this.cellArr[i]
-            this.cellArr[i] = this.cellArr[x]
-            this.cellArr[x] = temp
-        }
-        // console.log(this.cells)
-        // console.log('--------------------------------------------------------------------------------------------------'),
-    },
-
-    // distributes shuffled cells to grid of cell containers at the start of the game:
-    dealCells: function () {
-        // this.shuffleCells()
-        // linear array to apply image objects to div cell containers:
-        for (let k = 0; k < this.cellArr.length; k++) {
-            let newCell = this.cellArr[k];
-            // console.log(newCell)
-            // console.log(newCell.src)
-            document.getElementById(`cell${k}`).setAttribute('src', newCell.src)
-            document.getElementById(`cell${k}`).setAttribute('value', newCell.value)
-            document.getElementById(`cell${k}`).setAttribute('id', newCell.id)
-        }
-    },
-    // find the empty cell:
-    findEmpty: function(){
-        for(let i=0;i<this.gridArrLen;i++){
-            let tempCell = this.cellArr[i]
-            // console.log(tempCell.value)
-            if (tempCell.value == 0) {
-               this.emptyLoc=i
-            }
-        }
-    },
-
-    // adjacent cell variables:
-    // adjAbove: 0,
-    // adjBelow: 0,
-    // adjRight: 0,
-    // adjLeft: 0,
-
-    // adjAbove: this.emptyLoc-(Math.sqrt(this.gridArrLen)),
-    // adjBelow: this.emptyLoc+(Math.sqrt(this.gridArrLen)),
-    // adjRight: this.emptyLoc+1,
-    // adjLeft: this.emptyLoc-1,
-    
-
-    
-
-    // using emptyLoc, find adjacent (and clickable) cells:
-    findAdj: function(){
-        // figure out length of one side of grid:
-        let gridSide=Math.sqrt(this.gridArrLen)
-
-        // for (let i=0;i<this.gridArrLen;i++){
-            this.adjAbove=this.emptyLoc-gridSide
-            this.adjBelow=this.emptyLoc+gridSide
-            this.adjRight=this.emptyLoc+1
-            this.adjleft=this.emptyLoc-1
-
-
-
-
-
-        // }
-        
-        
-        // console.log(gridSide)
-        // if (this.emptyLoc)
-        // this.gridArrLen
-        // this.emptyLoc
     }
+};
 
+// populates cells array with object with image source, value, and id (is ID useful for the image?):
+function getCells() {
+// this.createCells()
+    for (let i = 1; i < 9; i++) {
+        cellArr.push({ src: `./images/image${i}.png`, value: i, id: `img${i}` })
+    }
+        cellArr.push({ src: `./images/empty.png`, value: 0, id: `img0` })
+        // console.log(cellArr)
+};
 
+// shuffles cells array so they are in random order, done at the start of the game:
+function shuffleCells () {
+    for (let i = (cellArr.length - 1); i > 0; i--) {
+        x = (Math.floor(Math.random() * (i)))
+        temp = cellArr[i]
+        cellArr[i] = cellArr[x]
+        cellArr[x] = temp
+    }
+};
 
+// distributes shuffled cells to grid of cell containers at the start of the game:
+function dealCells() {
+// linear array to apply image objects to div cell containers:
+    for (let i = 0; i < cellArr.length; i++) {
+        let newCell = cellArr[i];
+        document.getElementById(`cell${i}`).setAttribute('src', newCell.src)
+        document.getElementById(`cell${i}`).setAttribute('value', newCell.value)
+        document.getElementById(`cell${i}`).setAttribute('id', newCell.id)
+    }
+};
+
+// find the empty cell:
+function findEmpty() {
+    for (let i = 0; i < gridArrLen; i++) {
+        let tempCell = cellArr[i]
+        if (tempCell.value == 0) {
+            emptyLoc = i
+        }
+    }
 }
-//  ^^^^^^^ END OF GAME OBJECT- should be yellow
+
+// adjacent cell variables:
+// adjAbove: 0,
+// adjBelow: 0,
+// adjRight: 0,
+// adjLeft: 0,
+
+// adjAbove: this.emptyLoc-(Math.sqrt(this.gridArrLen)),
+// adjBelow: this.emptyLoc+(Math.sqrt(this.gridArrLen)),
+// adjRight: this.emptyLoc+1,
+// adjLeft: this.emptyLoc-1,
 
 
-game.fillGridArr()
-// console.log(game.gridArr)
 
-game.createCellDivs()
-game.getCells()
-game.shuffleCells()
-game.dealCells()
-game.findEmpty()
-// game.findAdj()
-console.log(`Empty location: ${game.emptyLoc}`)
-console.log(`Type of Empty location: ${typeof(game.emptyLoc)}`)
 
-// console.log(`Adjacent above cell: ${game.adjAbove}`)
-// console.log(`Adjacent below cell: ${game.adjBelow}`)
-// console.log(`Adjacent left cell: ${game.adjLeft}`)
-// console.log(`Adjacent right cell: ${game.adjRight}`)
+// using emptyLoc, find adjacent (and clickable) cells:
+// function findAdj(){
+
+//     // for (let i=0;i<this.gridArrLen;i++){
+//     adjAbove = emptyLoc - gridSide
+//     adjBelow = emptyLoc + gridSide
+//     adjRight = emptyLoc + 1
+//     adjleft = emptyLoc - 1
+
+
+
+
+
+//     // }
+
+
+//     // console.log(gridSide)
+//     // if (emptyLoc)
+//     // gridArrLen
+//     // emptyLoc
+// }
+
+
+
+
+
+fillGridArr()
+createCellDivs()
+getCells()
+shuffleCells()
+dealCells()
+findEmpty()
+// findAdj()
+console.log(`Empty location: ${emptyLoc}`)
+console.log(`Type of Empty location: ${typeof (emptyLoc)}`)
+
+// console.log(`Adjacent above cell: ${adjAbove}`)
+// console.log(`Adjacent below cell: ${adjBelow}`)
+// console.log(`Adjacent left cell: ${adjLeft}`)
+// console.log(`Adjacent right cell: ${adjRight}`)
 
 
 
@@ -242,49 +230,3 @@ document.getElementById("img4").classList.toggle("cell-Empty");
 // let dealButton = document.createElement('button');
 // dealButton.setAttribute("class", "dealButton");
 // document.body.appendChild(dealButton);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // James Sinkler  1:39 PM
-    // Coding Challenge: FizzBuzz
-    // 1-100, inclusive
-    // ===============================
-    // Find a javascript file, and try and write some code to do the following.
-    // If the number is divisible by 3, write “Fizz”
-    // If the number is divisible by 5, write “Buzz”
-    // If the number is divisible by both 3 and 5, write “FizzBuzz”
-    // Otherwise, write the number
-
-
-
-// const fizzBuzz=()=>{
-//     for (let i=1;i<101;i++){
-//         if (i%3===0 && i%5===0){
-//             console.log(`${i}, FizzBuzz`)            
-//         }else if (i%3==0){
-//             console.log(`${i}, Fizz`)
-//         }else if (i%5===0){
-//             console.log(`${i}, Buzz`)        
-//         } console.log(i)
-//     }    
-
-// }    
-
-// fizzBuzz()
-
