92d97209e89fc4729b66a2d61d1b7bbeccabfebe
enbre <noahbrezel@gmail.com>
2020-10-08 21:32:32 -0700

    Was able to identify a movable cell adjacent to empty (at index zero).

--- js/main.js
+++ js/main.js
@@ -30,21 +30,30 @@ container.setAttribute('class', 'container');
 document.querySelector('.game-frame').appendChild(container)
 
 
+//  2D array representing game board, but not actually used for the code: 
+let gridArr2 = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+
+// array representing the game board locations:
+let gridArr = [];
+// total number of cells in grid array
+let gridArrLen = 0;
+// length of one side of the grid
+let gridSide = Math.sqrt(gridArrLen);
+// array of inices along left column:
+let leftCol = [0, 3, 6];
+// array of inices along right column:
+let rightCol = [2, 5, 8];
+
+// array used to store image objects with source, value, and id keys:
+let cellArr = [];
+// grid index of empty cell:
+let emptyLoc = 0;
 
 // number of player moves:
-let moves= 0;
-// array representing the game board locations:
-let gridArr= [];
-// total number of cells in grid array
-let gridArrLen= 0;
-// length of one side of the grid
-let gridSide = Math.sqrt(gridArrLen)
-// array used to store image objects with source, value, and id keys:
-let cellArr= [];
-    
-let emptyLoc= 0;
+let moves = 0;
 
 // populate the grid array with indices:
+// Sets the overall size of game
 function fillGridArr() {
     for (let i = 0; i < 9; i++) {
         gridArr.push(i)
@@ -53,35 +62,35 @@ function fillGridArr() {
 };
 
 // creates divs containers for images:
-function createCellDivs () {
-// create cells
+function createCellDivs() {
+    // create cells
     for (let i = 0; i < 9; i++) {
         let cell = document.createElement('div');
-            cell.setAttribute('class', 'cell');
-            cell.setAttribute('id', `cellDiv${i}`)
+        cell.setAttribute('class', 'cell');
+        cell.setAttribute('id', `cellDiv${i}`)
         let cellImg = document.createElement('img')
-            cellImg.setAttribute('src', `./images/empty.png`);
-            cellImg.setAttribute('class', 'cell-Img');
-            cellImg.setAttribute('classList', 'cell-Empty');
-            cellImg.setAttribute('id', `cell${i}`)
-            container.appendChild(cell);
-            cell.appendChild(cellImg);
+        cellImg.setAttribute('src', `./images/empty.png`);
+        cellImg.setAttribute('class', 'cell-Img');
+        cellImg.setAttribute('classList', 'cell-Empty');
+        cellImg.setAttribute('id', `cell${i}`)
+        container.appendChild(cell);
+        cell.appendChild(cellImg);
 
     }
 };
 
 // populates cells array with object with image source, value, and id (is ID useful for the image?):
 function getCells() {
-// this.createCells()
+    // this.createCells()
     for (let i = 1; i < 9; i++) {
         cellArr.push({ src: `./images/image${i}.png`, value: i, id: `img${i}` })
     }
-        cellArr.push({ src: `./images/empty.png`, value: 0, id: `img0` })
-        // console.log(cellArr)
+    cellArr.push({ src: `./images/empty.png`, value: 0, id: `img0` })
+    // console.log(cellArr)
 };
 
 // shuffles cells array so they are in random order, done at the start of the game:
-function shuffleCells () {
+function shuffleCells() {
     for (let i = (cellArr.length - 1); i > 0; i--) {
         x = (Math.floor(Math.random() * (i)))
         temp = cellArr[i]
@@ -92,12 +101,16 @@ function shuffleCells () {
 
 // distributes shuffled cells to grid of cell containers at the start of the game:
 function dealCells() {
-// linear array to apply image objects to div cell containers:
+    // linear array to apply image objects to div cell containers:
     for (let i = 0; i < cellArr.length; i++) {
         let newCell = cellArr[i];
-        document.getElementById(`cell${i}`).setAttribute('src', newCell.src)
-        document.getElementById(`cell${i}`).setAttribute('value', newCell.value)
-        document.getElementById(`cell${i}`).setAttribute('id', newCell.id)
+        let tempCell = document.getElementById(`cell${i}`)
+        tempCell.setAttribute('src', newCell.src)
+        tempCell.setAttribute('value', newCell.value)
+        tempCell.setAttribute('id', newCell.id)
+        // tempCell.addEventListener('click', swapImg)
+        tempCell.addEventListener('click', isMovable)
+
     }
 };
 
@@ -105,112 +118,124 @@ function dealCells() {
 function findEmpty() {
     for (let i = 0; i < gridArrLen; i++) {
         let tempCell = cellArr[i]
-        if (tempCell.value == 0) {
+        if (tempCell.value === 0) {
             emptyLoc = i
+        }
+    }
+};
+
+
+
+// function isMovable(value) {
+function isMovable(e) {
+    if (emptyLoc===0){
+        let clickedCell =(parseInt(e.target.parentElement.id.charAt(    7   )  ) )
+            if (clickedCell===1 || clickedCell===3){
+                console.log('this cell is movable!')
+        // how to access image value as number:
+        // let clickedVal=(parseInt(e.target.id.charAt(3)))
         }
     }
 }
 
-// adjacent cell variables:
-// adjAbove: 0,
-// adjBelow: 0,
-// adjRight: 0,
-// adjLeft: 0,
 
-// adjAbove: this.emptyLoc-(Math.sqrt(this.gridArrLen)),
-// adjBelow: this.emptyLoc+(Math.sqrt(this.gridArrLen)),
-// adjRight: this.emptyLoc+1,
-// adjLeft: this.emptyLoc-1,
+// document.querySelectorAll('.cell-Img').forEach(e => {
+//     e.addEventListener('click', isMovable)
+// })
 
+// function swapImg(e) {
+// function swapImg(e) {
+//     // console.log(e.target)
+//     console.log(parseInt(e.target.id.charAt(3)))
+//     console.log(e.target.id)
+//     // console.log(e.target.value)
+//     // add empty image to clicked cell
+//     // isMovable(e.target.id)
+//     // and add image from clicked cell to empty cell
+//     // remove event listener from all cells
+// }
+
+
+
+
+
+
+
+// switch statements:
+// console.log(parseInt(value.charAt(3)))
+// switch (value) {
+// case 0:
+//     console.log('top left')
+//     break;
+// case 1:
+//     console.log('center top')
+//     break;
+// default:
+//     console.log('we should not get here')
+
+
+
+
+
+fillGridArr();
+createCellDivs()
+getCells()
+shuffleCells()
+dealCells()
+findEmpty()
+
+
+console.log(`Empty location: ${emptyLoc}`)
+console.table(gridArr2)
+
+
+
+
+// document.getElementById("img4").classList.toggle("cell-Empty");
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------
+
+// steps left to code:
+// apply 'click' listener event to adjacent cells to allow them to be clicked
+// click on div class, access child (img w values) using 'firstElementChild'
+// use set attribute to swap images:
+// clicked square to empty, empty square to image from clicked cell
+
+// for each move, increase the total number of moves made on the 'moves' tracker
+// once a clickable cell is clicked, swap images, value, and class with empty cell
+// once all cell's values are in order through the grid, the puzzle is solved
+// ------------------------------------------------
 
 
 
 // using emptyLoc, find adjacent (and clickable) cells:
 // function findAdj(){
 
-//     // for (let i=0;i<this.gridArrLen;i++){
-//     adjAbove = emptyLoc - gridSide
-//     adjBelow = emptyLoc + gridSide
-//     adjRight = emptyLoc + 1
-//     adjleft = emptyLoc - 1
+// // top row, don't include above location:
+// if (emptyLoc <=gridSide)
+
+// // bottom row, don't include below location:
+// if (emptyLoc>=gridArrLen-gridSide)
+
+// // left column, don't include left location
+// if (leftCol.includes(emptyLoc)===0)
+
+// // right column, don't include left location
+// if (rightCol.includes(emptyLoc)===0)
 
 
 
-
-
-//     // }
-
-
-//     // console.log(gridSide)
-//     // if (emptyLoc)
-//     // gridArrLen
-//     // emptyLoc
-// }
-
-
-
-
-
-fillGridArr()
-createCellDivs()
-getCells()
-shuffleCells()
-dealCells()
-findEmpty()
-// findAdj()
-console.log(`Empty location: ${emptyLoc}`)
-console.log(`Type of Empty location: ${typeof (emptyLoc)}`)
-
-// console.log(`Adjacent above cell: ${adjAbove}`)
-// console.log(`Adjacent below cell: ${adjBelow}`)
-// console.log(`Adjacent left cell: ${adjLeft}`)
-// console.log(`Adjacent right cell: ${adjRight}`)
-
-
-
-
-document.getElementById("img4").classList.toggle("cell-Empty");
-
-
-
-// add event listeners to determine if cells are movable
-// document.querySelectorAll('.cell-Img').forEach(e => {
-//     e.addEventListener('click', isNotMovable)
-//     function isNotMovable(e) {
-//         console.log('this cell is not movable')
-//     }
-// })
-
-
-// document.querySelectorAll('.cell-Adj').forEach(e =>{
-//     e.addEventListener('click', isMovable)
-//     function isMovable(j){
-//         console.log('this cell is movable')
-//     }
-// }),
-
-// document.querySelector('.cell-Empty').addEventListener('click', isEmpty)
-// function isEmpty(e) {
-//     console.log('this cell is empty')
-// }
-
-
-
-
-    // locate where the empty cell is on the grid
-// done
-    // by looping through a 2D array looking for the 'cell-Empty' class
-// done, via value of 0, not class
-    // locate which cells are adjacent to empty cell (above, right, left, bottom)
-    // based on 2D array empty cell location, each cell +/- 1 in X and Y axis
-    // done
-// change the class of those adjacent cells to '.cell-Adj' 
-// apply 'click' listener event to adjacent cells to allow them to be clicked
-// for each move, increase the total number of moves made on the 'moves' tracker
-// once a clickable cell is clicked, swap images, value, and class with empty cell
-// once all cell's values are in order through the grid, the puzzle is solved
-
-
+// from high card:
 
 // // Create Score Container
 // let scoreContainer = document.createElement('div');
@@ -230,3 +255,4 @@ document.getElementById("img4").classList.toggle("cell-Empty");
 // let dealButton = document.createElement('button');
 // dealButton.setAttribute("class", "dealButton");
 // document.body.appendChild(dealButton);
+
